ff099a6136cddd07d6c380dac39575c3fa22cba5
BartPlaza <grzebyk44@gmail.com>
2018-04-03 22:23:28 +0200

    next

--- Connector.php
+++ Connector.php
@@ -3,7 +3,7 @@
 class Connector {
 	public static function make($config){
 		try {
-			return new PDO("{$config['db_connection']};dbname={$config['db_name']};charset={$config['db_charset']}",$config['db_user'],$config['db_password'],$config['db_options']);
+			return new PDO("{$config['db_connection']};dbname={$config['db_name']};options={$config['db_charset_pg']}",$config['db_user'],$config['db_password'],$config['db_options']);
 		} catch (PDOException $e){
 			die($e->getMessage());
 		}
--- config.php
+++ config.php
@@ -2,11 +2,12 @@
 
 return [
 
-	'db_connection' => 'mysql:host=localhost',
-	'db_charset' => 'utf8',
-	'db_name' => 'tpt',
-	'db_user' => 'root',
-	'db_password' => '',
+	'db_connection' => 'pgsql:host=localhost',
+	'db_charset_mysql' => 'utf8',
+    'db_charset_pg' => '--client_encoding=UTF8',
+	'db_name' => 'php',
+	'db_user' => 'bart',
+	'db_password' => 'aaa123',
 	'db_options' => [
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
 	]
