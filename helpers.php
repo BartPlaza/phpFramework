6c47ec4f3a19f759226ed7287e8c068e08836502
Bartek <grzebyk44@gmail.com>
2018-04-04 11:58:35 +0200

    add_cookies

--- helpers.php
+++ helpers.php
@@ -6,10 +6,3 @@ function dd($item){
 	echo "</pre>";
 }
 
-function connectToDb(){
-	try {
-		return new PDO('mysql:host=localhost;dbname=tpt;charset=utf8','root', '');
-	} catch (PDOException $e){
-		die($e->getMessage());
-	}
-}
--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 	require_once 'vendor/autoload.php';
 	
 
+	if(!isset($_COOKIE['visits'])){
+		setcookie('visits', 1, time()+3600, '/');
+		echo "Welcome on our Page!";
+	} else {
+		if($_SERVER['REQUEST_URI'] === '/'){
+			setcookie('visits',$_COOKIE['visits'] += 1, time()+3600, '/'); 
+			echo "Welcome back! It is your {$_COOKIE['visits']} visit";
+		}
+	}
+
+	
 	
 	$router = require_once 'routes.php';
 
